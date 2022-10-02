561b710c45ee62bd698573b36308da64b957a80b
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2022-10-02 16:04:31 +0530

    Add files via upload

---
+++ AnalogClock/app.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    const hour = document.getElementById('hour');
+    const minute = document.getElementById('minute');
+    const second = document.getElementById('second');
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec  = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = sec_rotation = 6 * sec;
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);

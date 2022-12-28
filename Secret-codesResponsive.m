01b3812ee11c44ebfd6fa34a09f150b0a6149919
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2022-12-28 19:37:19 +0530

    Create Secret-codesResponsive.m

---
+++ Secret-codesResponsive.m
@@ -0,0 +1,11 @@
+position: absolute;
+  left: 0;
+  top: 250px;
+  right: 0;
+  text-align: center;
+  
+  
+  <!--for main container--->
+   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-gap: 100px;

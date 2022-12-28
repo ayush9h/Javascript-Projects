8aa2adbecfbfdb586ba7f13be13f56baec4c68b4
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2022-12-28 20:02:06 +0530

    Update Secret-codesResponsive.m

--- Secret-codesResponsive.m
+++ Secret-codesResponsive.m
@@ -9,3 +9,21 @@ position: absolute;
    display: grid;
   grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
   grid-gap: 100px;
+
+<!--for main button--->
+position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  display: inline-block;
+  background: aqua;
+  color: black;
+  font-size: 20px;
+  font-weight: 500;
+  padding: 10px 30px;
+  margin: 50px auto;
+  border-radius: 6px;
+  transition: all 0.3s ease;
+  border-radius: 0.2rem;
+  font-family: "Poppins", sans-serif;
